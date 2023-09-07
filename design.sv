fb0afae2ce07ec88525473b7a4e839cabea438ef
RITESH UPADHYAY <125303042+riteish06@users.noreply.github.com>
2023-09-07 18:15:37 +0530

    Add files via upload

--- design.sv
+++ design.sv
@@ -121,3 +121,21 @@ typedef enum logic [1:0] {
   
   
 endmodule
+///////////////////////////////////Interface Code
+
+
+
+interface abp_if;
+  
+  logic presetn;
+  logic pclk;
+  logic psel;
+  logic penable;
+  logic pwrite;
+  logic [31:0] paddr, pwdata;
+  logic [31:0] prdata;
+  logic pready, pslverr;  
+  
+  
+endinterface
+
